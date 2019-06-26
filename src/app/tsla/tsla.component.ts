b39f6e598a19d705a7e886487f3764b3bdb1365a
Makavura <makavuramughanga@gmail.com>
2019-06-26 12:26:20 +0300

    update components.ts

--- src/app/tsla/tsla.component.ts
+++ src/app/tsla/tsla.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { StockService } from '../_services/stock.service';
 
 @Component({
   selector: 'app-tsla',
@@ -7,9 +8,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TslaComponent implements OnInit {
 
-  constructor() { }
+  stockOpen: number[] = [];
+  stockClose: number[] = [];
+  dateInterval: string[] = [];
+  public lineChartData = [
+    { data: this.stockOpen, label: 'Open'},
+    { data: this.stockClose, label: 'Close'}
+    // { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
+    // { data: [330, 600, 260, 700, 250, 300, 590],  label: 'Account A' },
+    // { data: [120, 455, 100, 340, 170, 369, 288], label: 'Account B' },
+    // { data: [45, 67, 800, 500, 2, 909, 456], label: 'Account C' }
+  ];
+
+  public lineChartLabels = [];
+  public lineChartColors = [
+    {
+      borderColor: 'rgb(255, 10, 10)',
+    },
+    {
+      borderColor: 'rgb(83, 255, 9)',
+     },
+    {
+      borderColor: 'rgb(103, 58, 183)',
+     },
+    {
+      borderColor: 'black'
+    },
+  ];
+  public lineChartLegend = true;
+  public lineChartType = 'line';
+  public lineChartPlugins = [];
+
+  constructor(public stockService: StockService) { }
 
   ngOnInit() {
+    this.getStockData();
   }
 
+  getStockData() {
+    let stockData = [];
+    this.stockService.getMSFT().subscribe( data => {
+      stockData = data['Monthly Time Series'];
+      // tslint:disable-next-line: forin 
+      for (const item in stockData) {
+        this.dateInterval.push(item);
+        this.stockOpen.push(Number(stockData[item]['1. open']));
+        this.stockClose.push(Number(stockData[item]['4. close']));
+      }
+      this.lineChartLabels = this.dateInterval;
+
+    });
+
+  }
 }
