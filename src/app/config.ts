822319c9cd3f62fcc1872381d8c3d62ffa6df174
Makavura <makavuramughanga@gmail.com>
2019-06-26 12:31:47 +0300

    reflect new API in config.ts

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,3 +1,6 @@
 export const appConfig = {
-    MSFT: 'https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=MSFT&apikey=KPY48AEZJP6Y0D7O'
+    MSFT: 'https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=MSFT&apikey=KPY48AEZJP6Y0D7O',
+    GOOGL: 'https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=GOOGL&apikey=KPY48AEZJP6Y0D7O',
+    TSLA: 'https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=TSLA&apikey=KPY48AEZJP6Y0D7O',
+    AMZN: 'https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=AMZN&apikey=KPY48AEZJP6Y0D7O'
 };
