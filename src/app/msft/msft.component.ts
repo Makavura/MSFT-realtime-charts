b61afc11de25208c3a265e37551dc491f689416e
Makavura <makavuramughanga@gmail.com>
2019-06-27 10:25:01 +0300

    error flagging in devtools

--- src/app/msft/msft.component.ts
+++ src/app/msft/msft.component.ts
@@ -42,18 +42,25 @@ export class MsftComponent implements OnInit {
 
   getStockData() {
     let stockData = [];
+    // this.stockService.getMSFT().subscribe( data => {
+    //   stockData = data['Monthly Time Series'];
+    //   this.metaData = data['Meta Data'];
+    //   // tslint:disable-next-line: forin 
+    //   for (const item in stockData) {
+    //     this.dateInterval.push(item);
+    //     this.stockOpen.push(Number(stockData[item]['1. open']));
+    //     this.stockClose.push(Number(stockData[item]['4. close']));
+    //   }
+    //   this.lineChartLabels = this.dateInterval;
+
+    // });
     this.stockService.getMSFT().subscribe( data => {
-      stockData = data['Monthly Time Series'];
-      this.metaData = data['Meta Data'];
-      // tslint:disable-next-line: forin 
-      for (const item in stockData) {
-        this.dateInterval.push(item);
-        this.stockOpen.push(Number(stockData[item]['1. open']));
-        this.stockClose.push(Number(stockData[item]['4. close']));
-      }
-      this.lineChartLabels = this.dateInterval;
-
-    });
-
+      console.log('Success');
+     }
+     ,error => {
+       console.log('error on getMSFT()', error);
+       alert(error.message);
+     }
+     );
   }
 }
